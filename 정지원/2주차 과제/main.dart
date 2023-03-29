c07cbaff7930f603e95fb910010d4fce6b50d105
codeJiwon <loopy0107@sookmyung.ac.kr>
2023-03-29 23:45:23 +0900

    Feat: 2주차 과제

---
+++ 정지원/2주차 과제/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/material/colors.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xff2b80ff),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/AppsLogo.png',
+                    width: 200,
+                  ),
+                  const CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),
+                    backgroundColor: Colors.grey, strokeWidth: 10.0,)
+                ],
+              ),
+
+
+            ],
+
+
+          ),
+
+
+        ),
+      ),
+    );
+  }
+}
+
